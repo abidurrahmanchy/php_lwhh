8aeb5cc7109382c6c29523ef9d0a8521bc4fef69
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-04-09 20:29:14 +0600

    Introduction to variables
    Introduction to variables

---
+++ php02.php
@@ -0,0 +1,14 @@
+<?php
+$name='abidur rahman';
+$age= 21;
+$qustion='how are you?';
+echo "hello {$name},{$age},{$qustion}";
+echo "\n";
+echo "hello {$age},";
+echo "\n";
+echo "hello {$qustion}";
+echo "\n";
+echo "hello {$name}";
+echo "\n";
+
+
