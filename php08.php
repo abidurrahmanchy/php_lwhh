d38717e8ea0ec4b619d504be5f280b17e66b076f
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-04-20 18:21:28 +0600

    - ++ and - talk more about
    - ++ and - talk more about

---
+++ php08.php
@@ -0,0 +1,29 @@
+<?php
+/*$number = 12 + 3;
+operator & <operand>
+echo $number;*/
+ 
+/*$a = 2;
+$x = $y = $a;
+echo $x,"\n",$y;
+$number = 10;
+$number++;
+echo $number;*/
+
+/*$number =23;
+$m =$number++;
+echo $m,"\n",$number;
+
+$m =$number++;
+$m =$number;
+$number =$number +1;*/
+
+$number = 34;
+$m = ++$number;
+echo $m,"\n",$number;
+
+/*$m =++$number;
+$number =$number + 1;
+$m = $number;*/
+
+
