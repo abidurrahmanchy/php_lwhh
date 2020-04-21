015a1ed4376ca1d5553dc874bc008e2b2206bb96
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-04-21 10:08:19 +0600

    Discussion of different number systems
     Discussion of different number systems

---
+++ php09.php
@@ -0,0 +1,15 @@
+<?php
+$number = 123;
+$o = 012;
+$h = 0x2b;
+
+printf( "the number is %d and %d and %d", $number,$o ,$h);
+
+
+printf( "the binary equivate of %d is %b \n" ,44,44);
+
+printf( "the haxadcimal equivate of %d is %x \n" ,7645,7645);
+
+printf( "the octal equivate of %d is % o \n" ,1212, 1212);
+
+printf(" octal %o = %d" ,0020000,0020000 );
