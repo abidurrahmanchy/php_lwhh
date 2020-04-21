de28f9ffdbdef3fd7a72d56f2a484cd5f339fd03
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-04-21 20:47:42 +0600

    The magic of printf function
    The magic of printf function

---
+++ php10.php
@@ -0,0 +1,32 @@
+<?php
+$fname = nusrat;
+$Lname = rahman;
+
+printf( 'his name is %s %s',$fname,$Lname );
+echo"\n";
+printf( 'his name is %2$s %1$s',$fname,$Lname );//argument
+echo"\n";
+
+printf( "the binary equivaLent of %d is %b",23,23 );
+echo"\n";
+printf( 'the binary equivaLent of %1$d is %1$b', 445 );
+
+echo"\n";
+$number = 67.456;
+printf( "%.2f",$number);
+echo"\n";
+
+$m = 65;
+$number =98;
+printf("%04d \n",$m);
+printf("%04d \n",$number);
+echo "\n";
+
+$m = 67.8976;
+$number = 78.234;
+printf("%07.2f \n",$m);
+printf("%07.2f \n",$number);
+
+
+
+
