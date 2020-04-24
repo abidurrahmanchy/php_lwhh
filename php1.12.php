8b43662ffaaa42268f7c3f6ee9c3b5bc7ee117cb
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-04-24 13:12:30 +0600

    1.12 - Discussion of Conditions (Logic) and Logical Operators in PHP
    1.12 - Discussion of Conditions (Logic) and Logical Operators in PHP

---
+++ php1.12.php
@@ -0,0 +1,62 @@
+<?php
+$number =65;
+if($number % 2== 0 ){
+    echo "$number is and even number";
+}else {
+    echo"$number is and odd number";
+}
+echo "\n";
+if($number >23);{
+echo "$number is less than 23";
+}
+/*logical operators
+== 
+!=
+<
+<
+<=
+>=
+*/
+echo"\n";
+$number1 = 43;
+$m = 42;
+if( $m != $number);{
+echo"$m is not equal $numbeer";
+}
+echo "\n";
+$rahim =450;
+$kabbir =560;
+if($rahim == $kabbir){
+    echo"rahim has same amount money kabbir";
+
+}elseif($rahim > $kabbir){
+  echo"rahim has more money than kabbir";  
+
+}elseif( $rahim < $kabbir){
+    echo "rahim has less money than kabbir";
+}
+
+echo "\n";
+$age =11;
+if ($age >= 13 && $age <= 19){
+    echo"this person is a teenager";
+}else{
+    echo"this person is not a teenager";
+}
+echo"\n";
+$food = "tuna";
+if($food == "tuna"){
+    echo"it has vitamin D";
+}
+echo"\n";
+$food2 ="apple";
+if ($food2 == "apple"){
+    echo " apple has vitamin A";
+}
+?>
+
+
+
+
+
+
