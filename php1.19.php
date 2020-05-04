507c37c2db356975876cf1032316256eb8874bbc
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-04 10:09:24 +0600

    1.19 - Operator Presidency in PHP - who runs earlier 1.19 - Operator Presidency in PHP - who runs earlier
    1.19 - Operator Presidency in PHP - who runs earlier

---
+++ php1.19.php
@@ -0,0 +1,17 @@
+<?php
+$number= true || false;
+$string= true or false;
+var_dump( $number ,$string);
+
+echo"\n<br>";
+
+$number=  false || true;
+$string= false or true;
+var_dump( $number ,$string);
+
+echo"\n<br>";
+
+$number= true && false;
+$string= true and false;
+var_dump( $number ,$string);
+?>
