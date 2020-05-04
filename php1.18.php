5e3c8e88d738720d3e2345598e1e0044bb265638
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-04 10:07:19 +0600

    1.18 - An interesting issue of the switch case
    1.18 - An interesting issue of the switch case

---
+++ php1.18.php
@@ -0,0 +1,15 @@
+<?php
+$string ="37man";
+switch( $string){ 
+	case(string) "38mans":/*string not add: coed running result 37*/
+	echo "threetyeight man";
+	break;
+	case(string) 37:/*string not add. coed running result: 37*/
+	echo"37";
+	break;
+	case (string) "37mans":/*string not add: coed running result 37*/
+	echo "threetyseven mans";
+	break;
+
+}
+?>
