9cf53f3849566f02da1608149c7f2c97d4437567
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-04 12:41:41 +0600

    1.18 - An interesting issue of the switch case
    1.18 - An interesting issue of the switch case

--- php001.php
+++ php001.php
@@ -1,1 +1,16 @@
+<?php
+$string = 31;
+if( $string%2 ==0){
 	echo" {$string} is a  Evan Number";
+}else{
+	echo"{$string} is a Odd Number";
+}
+if ($string%2==0):
+
+    echo"($string) is a evan number";
+    echo PHP_EOL;
+    echo " some tex";
+else:
+    echo " ($string) is aodd number";
+    echo PHP_EOL;
+    echo " some tex++";    
