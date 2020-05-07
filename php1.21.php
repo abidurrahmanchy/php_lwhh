b5477dde1045034f68622403f8f1ac23026561cb
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-07 10:11:35 +0600

    1.21 - To clear some things about the ternary operator
    1.21 - To clear some things about the ternary operator

---
+++ php1.21.php
@@ -0,0 +1,11 @@
+<?php
+$string = 22;
+$result = ($string % 2==0) ? "A" : ( $string ==23) ? "B" :"C";
+echo $result;
+
+
+echo"\n.<br/>";
+$number=10;
+$result =( $number % 2==0) ? "A" : (($number ==11) ?"B" :"c");
+echo $result;
+?>
