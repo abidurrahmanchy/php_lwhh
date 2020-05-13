6a51a5a26b03597fc5a1a687cbd93dcaa5fb502b
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-13 22:16:29 +0600

    1.27 - with white loop ++ to keep in mind
    1.27 - with white loop ++ to keep in mind

---
+++ php1.27.php
@@ -0,0 +1,26 @@
+<?php
+$i =0;
+while ( $i<5){
+	echo $i;
+	echo"\n.</br>";
+	$i++;
+}
+echo " ======\n.</br>";
+$j = 0;
+while ( $j++ <5){
+	echo $j;
+	echo "\n.</br>";
+
+}
+echo"=======\n.</br>";
+$j =0;
+while ( ++$j <5){
+	echo $j;
+	echo "\n.</br>";
+
+} 
+echo " ======\n.</br>";
+$x = $y =5;
+$x = $y++;
+echo $x.":".$y;
+?>
