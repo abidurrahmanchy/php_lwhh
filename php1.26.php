931d53cc28b7b5098705b9c1cb3f1441566b45f0
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-14 10:47:20 +0600

    1.26 - More on Multiple Stepping in For Loop
    1.26 - More on Multiple Stepping in For Loop

--- php1.26.php
+++ php1.26.php
@@ -1,18 +1,18 @@
 <?php
 for ( $i =0 ;$i<100 ;$i+=7){
-echo $i."\n.</br>";
+echo $i ."\n</br>";
 }
 
 for ( $i =0; $i<100; $i++){
-	if ( $i % 7 ==0) echo $i."\n.</br>";
+	if ( $i % 7 ==0) echo $i. "\n</br>";
 	
-	if ( $i % 11 ==0) echo $i."\n.</br>";
+	if ( $i % 11 ==0) echo $i ."\n</br>";
 	
 }
 ?>
 <?php
 for ( $i =0, $j =0; $i<100; $i+=7, $j +=11){
-	echo $i ."\n.</br>";
-	echo $j <100 ? $j ."\n.</br>":'';
+	echo $i. "\n</br>";
+	echo $j <100 ? $j ."\n</br>":'';
 }
 ?>
