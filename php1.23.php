3d21ecf75481f48cbbcf2fa0e051fc76e72ce751
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-14 10:46:15 +0600

    1.23 - Multiple stepping for loop
    1.23 - Multiple stepping for loop

--- php1.23.php
+++ php1.23.php
@@ -1,18 +1,18 @@
 <?php
 for ( $i =10; $i >0; $i-=1){
 	echo $i;
-	echo "\n .</br>";
+	echo "\n </br>";
 }
 ?>
 <?php
 for ( $i =10; $i >0; $i-=1){
 	echo $i,":",(11-$i);
-	echo "\n.</br>";
+	echo "\n </br>";
 }
 
 for ( $i =10,$j=1; $i >0; $i-=1){
 	echo $i.":".$j;
-	echo "\n.</br>";
+	echo "\n </br>";
 }
 ?>
 
