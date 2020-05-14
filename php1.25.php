0cf0a6011bdf0d58b1a18c3e72aaa936c53baeff
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-14 10:46:45 +0600

    1.25 - Smart coding versus dumb coding - why you need to review your own code
    1.25 - Smart coding versus dumb coding - why you need to review your own code

--- php1.25.php
+++ php1.25.php
@@ -6,14 +6,14 @@ $j =1;
 for ( $string =1; $string <=10; $string++){
 	$result *=$string;
 	$j++;
-	echo " factorial of {$string} is {$result} \n.</br>";
+	echo " factorial of {$string} is {$result} \n </br>";
 }
-echo "total {$j} string of operations \n";
+echo "total {$j} string of operations \n </br>";
 
 for ( $i = $string; $i > 1; $i--){
 	$result *= $i;
 }
-echo " factorial of {$string} is {$result} \n.</br>";
+echo " factorial of {$string} is {$result} \n </br>";
 
 for ( $string = 1; $string<= 10; $string++){
 	$result = 1;
@@ -21,9 +21,9 @@ for ( $string = 1; $string<= 10; $string++){
 		$result*= $i;
 		$j++;
 	}
-	echo " factorial of {string} is {$result} \n.</br>";
+	echo " factorial of {string} is {$result} \n </br>";
 
 }
-echo "total {$j} string of operations \n.</br>";
+echo "total {$j} string of operations \n </br>";
 
 ?>
