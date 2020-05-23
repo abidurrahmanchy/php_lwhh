ce65117bf92b14ff6938773eecfe9818bf2b06a8
Md. Abidur Rahman Chowdhury <63197503+abidurrahmanchy@users.noreply.github.com>
2020-05-23 23:20:49 +0600

    2.9 - Variable Scope in PHP (Very Important)
    2.9 - Variable Scope in PHP (Very Important)

---
+++ php2.9.php
@@ -0,0 +1,40 @@
+<?php
+/*$name ="Hello World";//global scope
+function dosomething(){
+    global $name;
+    echo $name;
+    echo $GLOBALS ['name'];
+
+
+}
+dosomething();
+function doSomething (){
+    global $name;
+    $name = "How are you?";//local scope
+
+}
+dosomething();
+echo $name;*/
+function doSomething(){
+    //startc scope
+    static $i;
+    $i = $i ?? 0;
+    $i++;
+    echo "\n";
+}
+function doextra(){
+    static $i;
+    $i = $i ?? 0;
+    $i++;
+    echo $i;
+    echo "\n";
+}
+doSomething();
+doSomething();
+doSomething();
+doSomething();
+doExtra();
+doExtra();
+doSomething();
+doExtra();
+
